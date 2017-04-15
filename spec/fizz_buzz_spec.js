da08ff4459097befea5b1c8eba7b60ab07c0d065
junydania <o.dania@aol.com>
2017-04-15 20:37:15 +0200

    Test to check if number is divisible by 5 & 3

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -5,9 +5,16 @@ describe("Fizz_buzz", function() {
     subject = new fizzBuzz();
   });
 
-  it("Tests numbers divisible by ", function() {
-    var message = 'Fizz Buzz';
+  it("Tests numbers divisible by 15", function() {
     expect(subject.calculate_fizz(45)).toEqual('Fizz Buzz');
+  });
+
+  it("Tests numbers divisible by 5", function() {
+    expect(subject.calculate_fizz(5)).toEqual('Buzz');
+  });
+
+  it("Tests numbers divisible by 3", function() {
+    expect(subject.calculate_fizz(6)).toEqual('Fizz');
   });
 
 });
